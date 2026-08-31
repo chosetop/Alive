f49c1aacab7c37be390d2671e7138b86134e3204
刀妹大王 <2576003445@qq.com>
2026-08-31 11:24:37 +0800

    feat: add media registration service

--- backend/internal/media/model.go
+++ backend/internal/media/model.go
@@ -1,6 +1,10 @@
 package media
 
-import "errors"
+import (
+	"context"
+	"errors"
+	"time"
+)
 
 var ErrInvalidMime = errors.New("media: invalid mime type")
 var ErrInvalidSize = errors.New("media: invalid size")
@@ -9,6 +13,31 @@ const (
 	MaxImageBytes = 20 * 1024 * 1024
 	MaxVideoBytes = 500 * 1024 * 1024
 )
+
+type Media struct {
+	ID        int64
+	AuthorID  int64
+	ObjectKey string
+	MimeType  string
+	ByteSize  int64
+	CreatedAt time.Time
+}
+type Store interface {
+	Create(context.Context, Media) (Media, error)
+	Get(context.Context, int64) (Media, error)
+}
+type Service struct{ store Store }
+
+func NewService(store Store) *Service { return &Service{store: store} }
+func (s *Service) Register(ctx context.Context, m Media) (Media, error) {
+	if m.AuthorID <= 0 || m.ObjectKey == "" {
+		return Media{}, errors.New("media: invalid metadata")
+	}
+	if err := ValidateUpload(m.MimeType, m.ByteSize); err != nil {
+		return Media{}, err
+	}
+	return s.store.Create(ctx, m)
+}
 
 func ValidateUpload(mime string, size int64) error {
 	if size <= 0 {
